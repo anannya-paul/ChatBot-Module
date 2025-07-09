4838c12fd3c365761b33ef2a25ea650c992c39b6
Anannya Paul <81573374+anannya-paul@users.noreply.github.com>
2025-07-10 00:11:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const chatInput=document.querySelector(".chat-input textarea");
+const sendChatBtn=document.querySelector(".chat-input span");
+
+let userMessage;
+
+const  createChatLi=(message,className)=>{
+    const chatLi=document.createElement("li");
+    chatLi.classList.add("chat",className);
+    let chatContent=className==="outgoing"? `<p>${message}</p>`:`<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
+
+}
+
+const handleChat = ()=>{
+    userMessage=chatInput.value.trim();
+    if(!userMessage)
+    return;
+
+    createChatLi(userMessage,"outgoing");
+
+}
+
+sendChatBtn.addEventListener("click",handleChat);
